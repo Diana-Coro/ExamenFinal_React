a41b28f553f0881b2cc794c425083b8858360db3
alfredomc22071999-cyber <alfredomc22071999@gmail.com>
2026-07-03 19:40:54 -0400

    rayos

--- src/api/apiInstitucion.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const apiInstitucion = axios.create({
-  baseURL: "http://localhost:8080/api/instituciones",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
